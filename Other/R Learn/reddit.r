276a177bda0e6c3cd25c7d2094b26bf814694650
mnshr <mhira@alumni.nd.edu>
2017-02-16 18:58:40 -0500

    Init R

---
+++ Other/R Learn/reddit.r
@@ -0,0 +1,12 @@
+reddit <- read.csv('reddit.csv')
+
+table (reddit$employment.status)
+str(reddit)
+levels(reddit$age.range)
+levels(reddit$income.range)
+
+library(ggplot2)
+qplot(data=reddit, x=income.range)
+
+reddit$age.range <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "25-34", "35-44", "45-54", "55-64", "65 or Above" ))
+qplot(data=reddit, x=age.range)
